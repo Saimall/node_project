a344b57647350adb37394a45acec7808f45f824a
Saimall <113211361+Saimall@users.noreply.github.com>
2022-10-16 14:22:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+
+const express = require("express"); //importing express
+const app = express(); // creating new application
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { Todo } = require("./models");
+const todo = require("./models/todo");
+
+app.get("/todos", async (request, response) => {
+  // defining route to displaying message
+  console.log("Todo list");
+  try {
+    const todoslist = await Todo.findAll();
+    return response.send(todoslist);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("creating new todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      commpleted: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+//PUT https://mytodoapp.com/todos/123/markAscomplete
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("we have to update a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedtodo = await todo.markAsCompleted();
+    return response.json(updatedtodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+app.delete("/todos/:id", async (request, response) => {
+  console.log("delete a todo with ID:", request.params.id);
+  try {
+    const tododelete = await Todo.findByPk(request.params.id);
+    if (tododelete) {
+      await tododelete.delete();
+      await todo.destroy();
+      return response.json(true);
+    } else {
+      return response.json(false);
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+module.exports = app;
